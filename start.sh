774847b7c04aaf9d89b06714d8ed57a9c3f9af61
Oamau8 <97852875+Oamau8@users.noreply.github.com>
2022-01-19 18:25:20 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/Oamau8/streamuhng -b $BRANCH /streamuhng
 fi
-cd /VCPlayerBot
+cd /streamuhng
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
